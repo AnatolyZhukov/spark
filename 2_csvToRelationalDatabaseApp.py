83fde6119185bea95a8d4e3cc72810813f2a7896
Anatoliy Zhukov <anatoliy.zhukov@playgendary.com>
2022-03-25 16:24:44 +0300

    added show table in df from BQ table

--- 2_csvToRelationalDatabaseApp.py
+++ 2_csvToRelationalDatabaseApp.py
@@ -1,38 +1,21 @@
-'''
-  CSV to a relational database.
-'''
-
 from pyspark.sql import SparkSession
-from pyspark.sql import functions as F
-import os
-from google.cloud import bigquery
 
 
-current_dir = os.path.dirname(__file__)
-relative_path = "../../../USER/PycharmProjects/spark/data/authors.csv"
-absolute_file_path = os.path.join(current_dir, relative_path)
-
+relative_path = "data/authors.csv"
 # Creates a session on a local master
-spark = SparkSession.builder.appName("CSV to DB").master("local").getOrCreate()
-spark.conf.set("fs.gs.project.id", "test-bi")
+spark = SparkSession.builder.appName("CSV to DB").master("local"). \
+    config("spark.jars.packages", "com.google.cloud.spark:spark-bigquery-with-dependencies_2.12:0.23.2"). \
+    getOrCreate()
+
+spark.conf.set("fs.gs.project.id", "p-bi")
 spark.conf.set("google.cloud.auth.service.account.enable", "true")
-spark.conf.set("google.cloud.auth.service.account.json.keyfile", "../../../USER/test/gcp_acces")
+spark.conf.set("google.cloud.auth.service.account.json.keyfile", "/Users/user/Documents/GCP/gcp_acces_zhukov")
 
-#  Step 1: Ingestion
-#  ---------
-#
-#  Reads a CSV file with header, called authors.csv, stores it in a dataframe
-df = spark.read.csv(header=True, inferSchema=True, path=absolute_file_path)
+#df = spark.read.csv(header=True, inferSchema=True, path=relative_path)
 
-# Step 2: Transform
-# ---------
-# Creates a new column called "name" as the concatenation of lname, a
-# virtual column containing ", " and the fname column
-df = spark.read.format('bigquery').option('project','test-bi').option('table','test.applications').load()
+df = spark.read.format('bigquery').option('project','playgendary-bi').option('table','info.applications').load()
 df.show()
-# Step 3: Save
+#df.write.format("bigquery").option("temporaryGcsBucket","some-bucket").option("writeMethod", "direct").save("sandbox.author")
 
-# Saving the data to BigQuery
 
-# Good to stop SparkSession at the end of the application
 spark.stop()
